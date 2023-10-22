c7a2de9f59afd4a13ac36b83bffa87d328d7f471
lincolnyuji <63720047+Lincoln-Yuji@users.noreply.github.com>
2023-10-22 19:47:55 -0300

    Force PopulateView when scripts reload

--- Assets/Editor/VisualFluidBTView.cs
+++ Assets/Editor/VisualFluidBTView.cs
@@ -149,10 +149,4 @@ public class VisualFluidBTView : GraphView
         Debug.Log("Saved Tree");
         tree.SaveTree();
     }
-
-    [UnityEditor.Callbacks.DidReloadScripts]
-    private static void OnReloadScripts()
-    {
-        Debug.Log("Scripts foram recompilados...");
-    }
 }
